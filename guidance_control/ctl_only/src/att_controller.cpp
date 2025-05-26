5ee4d37b3fd610289ffb7a19fef5d8e097aa32ee
MrKeith99 <kvalentincardenas@hotmail.com>
2025-05-26 12:56:54 +0900

    [FIX] format from tab to space

--- guidance_control/ctl_only/src/att_controller.cpp
+++ guidance_control/ctl_only/src/att_controller.cpp
@@ -6,15 +6,15 @@
 // ファイルスコープ
 namespace
 {
-	/** 符号処理
-	 * @param [in] a 検査値
-	 * @retval 1. aがゼロ以上
-	 * @retval -1. aが負
-	 */
-	double sign(double a)
-	{
-		return a >= 0 ? 1. : -1.;
-	}
+    /** 符号処理
+     * @param [in] a 検査値
+     * @retval 1. aがゼロ以上
+     * @retval -1. aが負
+     */
+    double sign(double a)
+    {
+        return a >= 0 ? 1. : -1.;
+    }
 }
 
 //------------------------------------------------------------------------------
@@ -28,20 +28,20 @@ kp_(1.), kd_(1.)
 // rosparamによるコンストラクタ
 ib2::AttController::AttController(const ros::NodeHandle& nh)
 {
-	using namespace ib2_mss;
-	
-	double kp(-1.);
-	double kd(-1.);
-	nh.getParam("/att_ctl/kp"   , kp);
-	nh.getParam("/att_ctl/kd"   , kd);
-	RangeCheckerD::notNegative(kp, true, "kp");
-	RangeCheckerD::notNegative(kd, true, "kd");
-	kp_ = kp;
-	kd_ = kd;
+    using namespace ib2_mss;
+    
+    double kp(-1.);
+    double kd(-1.);
+    nh.getParam("/att_ctl/kp"   , kp);
+    nh.getParam("/att_ctl/kd"   , kd);
+    RangeCheckerD::notNegative(kp, true, "kp");
+    RangeCheckerD::notNegative(kd, true, "kd");
+    kp_ = kp;
+    kd_ = kd;
 
-	ROS_INFO("******** Set Parameters in att_controller.cpp");
-	ROS_INFO("/att_ctl/kp   : %f", kp_);
-	ROS_INFO("/att_ctl/kd   : %f", kd_);
+    ROS_INFO("******** Set Parameters in att_controller.cpp");
+    ROS_INFO("/att_ctl/kp   : %f", kp_);
+    ROS_INFO("/att_ctl/kd   : %f", kd_);
 }
 
 //------------------------------------------------------------------------------
@@ -69,14 +69,14 @@ ib2::AttController& ib2::AttController::operator=(AttController&&) = default;
 //  比例ゲインの取得
 double ib2::AttController::kp() const
 {
-	return kp_;
+    return kp_;
 }
 
 //------------------------------------------------------------------------------
 //  微分ゲインの取得
 double ib2::AttController::kd() const
 {
-	return kd_;
+    return kd_;
 }
 
 //------------------------------------------------------------------------------
@@ -85,16 +85,16 @@ Eigen::Vector3d ib2::AttController::torqueCommand
 (const Eigen::Quaterniond &q, const Eigen::Vector3d &w, const CtlElements &p,
  const Eigen::Matrix3d& Is) const
 {
-	// 誤差量計算
-	Eigen::Quaterniond qe = p.q().conjugate()*q;
+    // 誤差量計算
+    Eigen::Quaterniond qe = p.q().conjugate()*q;
 
-	// ωc計算
-	Eigen::Vector3d    wc    = -2.0 * kp_ / kd_ * sign(qe.w())
-			* qe.vec() + qe.conjugate() * p.w();
+    // ωc計算
+    Eigen::Vector3d    wc    = -2.0 * kp_ / kd_ * sign(qe.w())
+            * qe.vec() + qe.conjugate() * p.w();
 
-	// 姿勢制御則
-	Eigen::Vector3d troque = kd_ * Is * (wc - w) + w.cross(Is * w);
+    // 姿勢制御則
+    Eigen::Vector3d troque = kd_ * Is * (wc - w) + w.cross(Is * w);
 
-	return troque;
+    return troque;
 }
 // End Of File -----------------------------------------------------------------
