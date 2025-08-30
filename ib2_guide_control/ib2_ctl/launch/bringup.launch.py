425c9cc9f1669d35f3ac2a84564c5cdfc1f275b4
MrKeith99 <kvalentincardenas@hotmail.com>
2025-08-30 19:28:48 +0900

    [ADD] ros2 launcher

--- ib2_guide_control/ib2_ctl/launch/bringup.launch.py
+++ ib2_guide_control/ib2_ctl/launch/bringup.launch.py
@@ -27,7 +27,7 @@ def launch_setup(context, *args, **kwargs):
   # Get the share directory of the ctl_only and prop packages
   ctl_only_share_dir = get_package_share_directory('ib2_ctl')
   fsm_share_dir = get_package_share_directory('ib2_fsm')
-  prop_share_dir = get_package_share_directory('prop')
+  prop_share_dir = get_package_share_directory('ib2_prop')
 
   # Construct the full path to the YAML configuration files
   ctl_yaml = os.path.join(ctl_only_share_dir, 'config', 'ctl.yaml')
@@ -40,24 +40,29 @@ def launch_setup(context, *args, **kwargs):
       package='ib2_ctl',
       executable='ib2_ctl_node',
       name='ctl_only',
-      parameters=[ctl_yaml]
+      parameters=[ctl_yaml,
+        {
+          'use_sim_time': True
+        }
+      ]
     ),
-    Node(
-      package='ib2_prop',
-      executable='ib2_prop_node',
-      name='prop',
-      parameters=[prop_yaml]
-    ),
+    # Node(
+    #   package='ib2_prop',
+    #   executable='ib2_prop_node',
+    #   name='prop',
+    #   parameters=[prop_yaml]
+    # ),
   ])
 
-  # If use_fsm is true, add the fsm node
-  if use_fsm.lower() == 'true':
-    nodes_to_launch.extend([
-      Node(
-        package='ib2_fsm',
-        executable='ib2_fsm_node',
-        name='fsm'
-      ),
-    ])
+  # # If use_fsm is true, add the fsm node
+  # if use_fsm.lower() == 'true':
+  #   nodes_to_launch.extend([
+  #     Node(
+  #       package='ib2_fsm',
+  #       executable='ib2_fsm_node',
+  #       name='fsm',
+  #       parameters=[ctl_yaml]
+  #     ),
+  #   ])
 
   return nodes_to_launch
